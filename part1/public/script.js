a3b3ac2b6983f98ca227d6ceaf2c4735b077f423
neon <saahilattud@gmail.com>
2021-08-21 09:01:42 -0400

    erer

--- part1/public/script.js
+++ part1/public/script.js
@@ -6,3 +6,13 @@ fetch('/date').then(d => {
         document.getElementById('date').innerHTML = data
     })
 })
+fetch('/info').then(d => d.json()).then(data => {
+    data.forEach((d, i) => {
+        console.log(i)
+        let textId = document.getElementById(`info-${i}-text`)
+        let titleId = document.getElementById(`info-${i}-title`)
+        textId.innerHTML = d.description
+        titleId.innerHTML = d.title
+    })
+    
+})
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,8 +1,7 @@
-const logger = require('./structures/logger')
-const insLogger = new logger(__dirname + '/logs')
+
 const App = require('./structures/app')
-let { log, debug, error } = insLogger
-if(!process.env?.IS_ENV) {
+let { log, debug, error } = console
+if(!process.env.IS_ENV) {
     require('dotenv').config()
 debug('Enabled dotenv')
 }
@@ -10,17 +9,17 @@ log('Loading index.js')
 let info = {
     title: 'about me',
     name: 'Saahil',
-    description: ' is a dev'
+    description: ' is a dev',
+    tabs: [{ title: 'Info 1', description: '...info'.repeat(3) }, { title: 'Info 2', description: '...info'}]
 }
 // let questions = require('./questions.json')
-let { app } = new App(insLogger)
+let { app } = new App(console)
 app.get('/', (req,res) => {
-    res.render('index', { info })
+    res.render('index', { info, req,res })
 })
 app.get('/date', (req,res) => {
     res.send(new Date)
 })
-insLogger.on('ready', () => log('ready'))
-insLogger.on('debug', text => {
-    debug(text)
+app.get('/info', (req,res) => {
+    res.json(info.tabs)
 })
